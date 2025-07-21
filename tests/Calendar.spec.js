0e27cc45593893d045ec642e6de6d27e3be57a9a
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-07-21 22:43:40 +1200

    - Validates month, date, and year input values against expected list - Used for loop to iterate through calendar input fields - Ensures UI reflects correct date selection

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -1,19 +1,27 @@
-const {test, expect} = require("@playwright/test");
+const {test, expext, expect} = require ('@playwright/test');
 
+test ('Calendar Validations', async ({page}) => {
 
-test("Calendar validations", async ({page}) => {
-  
-    
-    const monthNumber = "6";
+    const month = "6";
     const date = "15";
     const year = "2027";
+    const expectedList = [month, date, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-    await page.locator(".react-date-picker__inputGroup").click();
-    await page.locator(".react-calendar__navigation__label").click();
-    await page.locator(".react-calendar__navigation__label").click();
+    await page.locator('.react-date-picker__inputGroup').click();
+    await page.locator('.react-calendar__navigation__label').click();
+    await page.locator('.react-calendar__navigation__label').click();
     await page.getByText(year).click();
-    await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click();
-    await page.locator("//abbr[text()='"+date+"']").click();
+    await page.locator('.react-calendar__year-view__months__month').nth(Number(month)-1).click();
+    await page.locator(`//abbr[text()='${date}']`).click();
+    const inputs = await page.locator('.react-date-picker__inputGroup input');
+    for (let index = 0; index < inputs.length; index++) { 
+        const dateFromtheBrowser = inputs[index].getAttribute("value");
+        expect(dateFromtheBrowser).toEqual(expectedList[index]);
+    }
 
-})
+
+
+});
+
+
---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,21 @@
+const {test, expect} = require ('@playwright/test');
+
+test("Pop up validations", async ({page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+    // await page.goto("https://binaryville.com/");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    await page.pause();
+    await page.locator('#confirmbtn').click();
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator('#mousehover').hover();
+    const framesPage = page.frameLocator("#courses-iframe");
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framesPage.locator(".text h2").textContent();
+    console.log(textCheck.split(" ")[1]);
+});
